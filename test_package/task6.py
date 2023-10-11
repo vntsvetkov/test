64407aefeb19d837e4f4bbf28bb4a565e26169e3
Goryachev k.s <yandex.kost@yandex.ru>
2023-10-11 20:20:18 +0300

    Заполнил функцию reverse_name_file

--- test_package/task6.py
+++ test_package/task6.py
@@ -2,5 +2,8 @@
 Напишите программу, которая принимает имя файла с расширением и возвращает его имя в перевернутом виде.
 """
 
+
 def reverse_name_file(name_file: str) -> str:
-    ...
+    index = name_file.find(".")
+    revers_name = name_file[index - 1:: -1] + name_file[index:]
+    return revers_name
