26c00d5472cc81137016ba40c6246266f9007117
Andrey Skornyakov <dyx76@yandex.ru>
2023-10-11 20:10:58 +0300

    Написал алгоритм нахождения чисел в списках

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+"""
+Напишите функцию, которая принимает два списка и возвращает все целочисленные элементы первого и второго списка
+в порядке убывания
+"""
 
 
-def main():
-    print("Hello, it's Tsvetkov Nikolay!")
+def get_numbers(a: list, b: list) -> list | None:
+    a.extend(b)
+    c = []
+    for elem in a:
+        if str(elem).isnumeric():
+            c.append(int(elem))
+    if len(a):
+        return c
+    else:
+        return None
 
 
 if __name__ == "__main__":
